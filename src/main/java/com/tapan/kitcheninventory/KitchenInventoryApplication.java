fdd6f6db22de1e571f26fbd00281f4b33cd49300
Tapan Behera <tapankumarbehera@gmail.com>
2019-05-15 22:52:45 +0530

    creating the kitchen-inventory project

---
+++ src/main/java/com/tapan/kitcheninventory/KitchenInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.tapan.kitcheninventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenInventoryApplication.class, args);
+	}
+
+}
